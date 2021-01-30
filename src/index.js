5f38ad156fb3b6df93eaed9048867994510c5277
Ivan Fediakov <ivan_fediakov@epam.com>
2021-01-30 16:37:26 +0300

    feat: finish modal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.scss';  
 
-$('.card__button').on('click', () => {
-    console.log('in');
-    $('#login-form').modal();
+$('.card__button').on('click', function() {
+    $('#order-modal').modal();
+
+    const $button = $(this);
+
+    const orderName = $button.data('order-name');
+    const $modalTitle = $('.order-modal__title');
+    $modalTitle.text(orderName);
+
+    const $buttonWrapper = $('.form__submit-button');
+    switch (orderName) {
+        case 'Студент':
+            $buttonWrapper.addClass('orange'); 
+            break;
+    
+        case 'Профессионал':
+            $buttonWrapper.addClass('purple'); 
+            break;
+
+        case 'Учёный':
+            $buttonWrapper.addClass('mint'); 
+            break;
+
+        default:
+            break;
+    }
+
     return false;
 });

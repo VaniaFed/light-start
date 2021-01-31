1aab1f1aefa845dd6ed99793d1bd247b54cd85f8
Ivan Fediakov <ivan_fediakov@epam.com>
2021-01-31 20:00:16 +0300

    feat: footer; header adaptation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.scss';  
 
+$('.main-block button').on('click', function() {
+    $('#order-modal-main').modal();
+
+    const $button = $(this);
+
+    const orderName = $button.data('order-name');
+    const $modalTitle = $('.order-modal__title');
+    $modalTitle.text(orderName);
+
+    const $buttonWrapper = $('.form__submit-button');
+    $buttonWrapper.addClass('purple'); 
+
+    return false;
+});
+
 $('.card__button').on('click', function() {
     $('#order-modal').modal();
 
